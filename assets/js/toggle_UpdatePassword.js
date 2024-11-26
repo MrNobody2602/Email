0ec0d9a50521a322b3b566a6453b7e4cda10a4dc
MrNobody2602 <MrNobody2602@users.noreply.github.com>
2024-11-26 19:41:35 +0800

    Updated toggle

--- assets/js/toggle_UpdatePassword.js
+++ assets/js/toggle_UpdatePassword.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $('#toggleCurrentPassword').on('click', function() {
-        // Toggle the password input type{green,0}
+        // Toggle the password input type
         const passwordInput = $('#currentPassword');
         const type = passwordInput.attr('type') === 'password' ? 'text' : 'password';
         passwordInput.attr('type', type);
@@ -10,12 +10,12 @@ $(document).ready(function(){
     });
 
     $('#toggleNewPassword').on('click', function() {
-        // Toggle the confirm password input type{green,0}
+        // Toggle the confirm password input type
         const confirmPasswordInput = $('#newPassword');
         const type = confirmPasswordInput.attr('type') === 'password' ? 'text' : 'password';
         confirmPasswordInput.attr('type', type);
         
-        // Toggle the eye icon{green,0}
+        // Toggle the eye icon
         $(this).toggleClass('fa-eye fa-eye-slash');
     });
 });
